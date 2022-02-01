e1632688d897ebaed2e522f9ad8dac95ebe7a2c9
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-01-31 22:04:30 -0300

    Conclusão da estrutura inicial + Funcionalidade de adicionar tarefas

--- js/main.js
+++ js/main.js
@@ -1,28 +1,50 @@
 const Main = {
-  
   init: function () {
     this.cacheSelectors()
     this.bindEvents()
   },
-  
+
+
   cacheSelectors: function () {
-    this.checkButtons = document.querySelectorAll('.check')
+    this.$checkButtons = document.querySelectorAll('.check')
+    this.$inputTask = document.querySelector('#inputTask')
   },
+
 
   bindEvents: function () {
     const self = this
 
-    this.checkButtons.forEach(function(button) {
+    this.$checkButtons.forEach(function (button) {
       button.onclick = self.Events.checkButton_click
     })
+
+    this.$inputTask.onkeypress = self.Events.inputTask_keypress
   },
 
-  Events: {
-    checkButton_click: function () {
-      alert('ok')
-    }
-  }
 
+  Events: {
+    checkButton_click: function (e) {
+      const li = e.target.parentElement
+      const isDone = li.classList.contains('done')
+
+      if (!isDone) {
+        return li.classList.add('done')
+      }
+
+      li.classList.remove('done')
+    },
+    
+    inputTask_keypress: function (e) {
+      const key = e.key
+      const value = e.target.value
+
+      if (key === 'Enter') {
+        console.log('deu certo o enter')
+      }
+    }
+
+
+  }
 }
 
 Main.init()
