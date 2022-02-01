7640fc4f808f2a22feebc9672e5d43db5c36117f
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-01-31 21:37:05 -0300

    Estrutura inicial(Main) javaScript do projeto ToDoList

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-console.log('projeto to do list')
+const Main = {
+  
+  init: function () {
+    this.cacheSelectors()
+    this.bindEvents()
+  },
+  
+  cacheSelectors: function () {
+    this.checkButtons = document.querySelectorAll('.check')
+  },
+
+  bindEvents: function () {
+    const self = this
+
+    this.checkButtons.forEach(function(button) {
+      button.onclick = self.Events.checkButton_click
+    })
+  },
+
+  Events: {
+    checkButton_click: function () {
+      alert('ok')
+    }
+  }
+
+}
+
+Main.init()
